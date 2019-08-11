2288eb4c4374bfc1346bb5835feb8c40e1b313f0
Animesh <animesh.ultimate@gmail.com>
2019-08-11 18:02:35 +0530

    users with uploading feature working! added user has no role if user has not

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -20,7 +20,7 @@
                         <td>{{ $user->id }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
-                        <td>{{ $user->role->name }}</td>
+                        <td>{{ $user->role ? $user->role->name : 'User has no role' }}</td>
                         <td>{{ $user->is_active == 1 ? 'Active' : 'Not Active' }}</td>
                         <td>{{ $user->created_at->diffForHumans() }}</td>
                         <td>{{ $user->updated_at->diffForHumans() }}</td>
